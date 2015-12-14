919555455f4aff6cda1be0c6e69c2ffc12413595
Alex Klimovich <alex_klimovich@yahoo.com>
2015-12-15 00:55:35 +0300

    #7 implment `fillable` decorator

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,20 +1,23 @@
-from mock import Mock
+from mock import Mock, call
+from nose.tools import eq_
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
+from autoui.decorators.abstract import fillable
 from autoui.decorators.common import until_presence_of_element_located
 from autoui.driver import get_driver
 from autoui.elements.abstract import Element
+from autoui.elements.common import Input
 from autoui.locators import XPath
 from tests.base import BaseTestCase
 
 
-class TestDecorators(BaseTestCase):
+class TestUntil(BaseTestCase):
     def setUp(self):
-        super(TestDecorators, self).setUp()
+        super(TestUntil, self).setUp()
         get_driver._driver.find_element = Mock(side_effect=[NoSuchElementException(), self.web_element])
 
     def tearDown(self):
-        super(TestDecorators, self).tearDown()
+        super(TestUntil, self).tearDown()
 
     def test_until(self):
         class Page(object):
@@ -30,3 +33,69 @@ class TestDecorators(BaseTestCase):
         p = Page()
         e = p.element
         assert e.web_element is self.web_element
+
+
+class TestFillableDecorator(BaseTestCase):
+    # same as in `Element` test
+    # but with decorator `fillable`
+    def test_inherited_fillable_elements(self):
+        @fillable
+        class Section2(Element):
+            s2_el1 = Input(XPath('s2_el1'))
+            s2_el2 = Input(XPath('s2_el2'))
+
+        @fillable
+        class Section1(Element):
+            locator = XPath('section1')
+            section2 = Section2(XPath('section2'))
+            s1_el1 = Input(XPath('s1_el1'))
+
+        class Page(object):
+            section1 = Section1()
+
+        # basic test
+        section1 = Page.section1
+        assert isinstance(section1, Section1)
+        assert section1.web_element is self.web_element
+
+        dict_to_fill = {
+            's1_el1': 's1_el1_value',
+            'section2': {
+                's2_el1': 's2_el1_value',
+                's2_el2': 's2_el2_value',
+            }
+        }
+        section1.fill(dict_to_fill)
+
+        # section1 must be found with driver
+        self.driver.find_element.assert_has_calls([call('xpath', 'section1')])
+
+        # section2 and items of section1 must be found with web element of section1
+        self.web_element.find_element.assert_has_calls([
+            call('xpath', 's1_el1'),
+            call('xpath', 'section2')
+        ], any_order=True)
+
+        self.web_element_inh.assert_has_calls([
+            call.clear(),
+            call.send_keys('s1_el1_value'),
+        ])
+
+        # items of section2 must be found with web element of section2
+        self.web_element_inh.find_element.assert_has_calls([
+            call('xpath', 's2_el1'),
+            call('xpath', 's2_el2'),
+        ], any_order=True)
+
+        self.web_element_inh_2.assert_has_calls([
+            call.clear(),
+            call.send_keys('s2_el1_value'),
+            call.clear(),
+            call.send_keys('s2_el2_value')
+        ])
+
+        self.web_element_inh.get_attribute.return_value = 's1_el1_value'
+        self.web_element_inh_2.get_attribute.side_effect = ['s2_el1_value', 's2_el2_value']
+
+        state = section1.get_state()
+        eq_(dict_to_fill, state)
